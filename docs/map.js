353536c1d28fddb6ba7190c5dcee2d29dda9cb49
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-02-27 09:32:03 +0900

    Update map.js

--- docs/map.js
+++ docs/map.js
@@ -9,7 +9,7 @@ function setClickEvent() {
     var lands = document.getElementsByClassName('land');
     Array.from(lands).forEach(land => land.onclick = function() {
         var code = this.className.baseVal.split(' ')[1].toUpperCase();
-        console.log(codes[code]);
+        wkiFrame.src = 'https://ja.wikipedia.org/wiki/' + codes[code];
     });
 }
 
