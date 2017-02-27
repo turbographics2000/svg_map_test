09dc18edc8be84b0062478138bdb280a94961438
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-02-27 10:33:41 +0900

    Update map.js

--- docs/map.js
+++ docs/map.js
@@ -6,7 +6,7 @@ fetch('BlankMap-World6-Equirectangular.svg').then(res => res.text()).then(svgTex
 function setClickEvent() {
     var paths = document.getElementsByTagName('path');
     Array.from(paths).forEach(path => path.onclick = function(evt) {
-        var code = this.id.length === 2 ? this.id : this.parent.classList[this.classList.length - 1].toUpperCase();
+        var code = (this.id.length === 2 ? this.id : this.parent.classList[this.classList.length - 1]).toUpperCase();
         wikiFrame.src = 'https://ja.wikipedia.org/wiki/' + codes[code];
     });
 }
