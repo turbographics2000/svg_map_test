b3ccaeb85c929de27cfdd42b3c3d034086291dc0
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-02-27 09:21:35 +0900

    Update map.js

--- docs/map.js
+++ docs/map.js
@@ -7,10 +7,10 @@ fetch('BlankMap-World6-Equirectangular.svg').then(res => res.text()).then(svgTex
 
 function setClickEvent() {
     var land = document.getElementsByClassName('land');
-    land.onclick = function() {
+    land.addEventListener('cick', function() {
         var code = this.className.split(' ')[0].toLowerCase();
         console.log(codes[code]);
-    }
+    });
 }
 
 var codes = {
