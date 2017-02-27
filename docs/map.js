93cfe984b53733eadaea8e700873c11696757574
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-02-27 09:11:16 +0900

    Update map.js

--- docs/map.js
+++ docs/map.js
@@ -1,4 +1,5 @@
 fetch('BlankMap-World6-Equirectangular.svg').then(res => res.text()).then(svgText => {
     var svg = document.createElement('svg');
     svg.outerHTML = svgText;
+    document.body.appendChild(svg);
 });
